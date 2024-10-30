dae4e916aeeab594036d64bc071e99d1f5ac0cd1
Thomas-Marino <thomasmarino627@gmail.com>
2024-10-30 02:41:27 -0300

    Implementacion de rutas ingreso, registro y landing-page

--- ThonicSalud/src/app/app-routing.module.ts
+++ ThonicSalud/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { IngresoComponent } from './components/ingreso/ingreso.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/landing", pathMatch: "full" },
+  { path: "landing", component: LandingPageComponent },
+  { path: "ingreso", component: IngresoComponent },
+  { path: "registro", component: RegistroComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
