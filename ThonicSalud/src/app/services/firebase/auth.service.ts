40ec4c5e9702ceeb5cd75883e914ca212ee4cc77
unknown <thomasmarino627@gmail.com>
2024-10-30 18:18:36 -0300

    Mejoras en el código + implementación de registro

--- ThonicSalud/src/app/services/firebase/auth.service.ts
+++ ThonicSalud/src/app/services/firebase/auth.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { getAuth, User } from '@angular/fire/auth'
+import { ActionCodeInfo, ActionCodeSettings, getAuth, sendEmailVerification, User, UserCredential } from '@angular/fire/auth'
 import { FirebaseError } from '@angular/fire/app';
 import { Router } from '@angular/router';
 import { BehaviorSubject, firstValueFrom, Observable } from 'rxjs';
@@ -35,22 +35,67 @@ export class AuthService {
 				authResponse.mensajeError = error.message;
 			});
 
-			if(loginResponse)
+			if(loginResponse && loginResponse.user?.emailVerified)
 			{
 				console.log(`Usuario logueado existosamente! Correo: ${loginResponse.user?.email}`);
 				authResponse.mensajeExito = "Ingreso de usuario exitoso! Redirigiendo a la pantalla de inicio...";
 				localStorage.setItem("usuarioLogueado", correo.split("@")[0])
 			}
-
-			return authResponse;
+			else if(!loginResponse)
+			{
+				console.error(`Usuario inexistente!`);
+				authResponse.huboError = true;
+				authResponse.mensajeError = "El usuario no existe en el listado de usuarios registrados.";
+			}
+			else if(loginResponse && !loginResponse.user?.emailVerified)
+			{
+				console.error(`Correo de usuario no verificado!`);
+				authResponse.huboError = true;
+				authResponse.mensajeError = "El usuario no verificó su email aún.";
+			}
 		}
 		catch(error:any)
 		{
 			console.error(error)
 			authResponse.huboError = true;
 			authResponse.mensajeError = error;
-			return authResponse;
 		}   
+		finally { return authResponse; }
+	}
+
+	async RegistrarUsuario(correo: string, password: string): Promise<authResponse>
+	{
+		let authResponse: authResponse = {huboError: false};
+		try 
+		{
+			const regResponse = await this.auth.createUserWithEmailAndPassword(correo, password).catch((error:FirebaseError) => {
+				if(error.code == "auth/invalid-email") { error.message = "Asegurese de ingresar un email válido (ejemplo@correo.com)"; }
+				else if(error.code == "auth/missing-password") { error.message = "Asegurese de ingresar una contraseña"; }
+				else if(error.code == "auth/email-already-in-use") { error.message = "El correo ingresado ya está en uso!"; }
+				error.stack = ""; 
+				authResponse.huboError = true;
+				authResponse.mensajeError = error.message;
+			});
+			
+			if(regResponse && regResponse.user) 
+			{ 
+				console.log(`Usuario creado exitosamente!`);
+				await sendEmailVerification(regResponse.user);
+				authResponse.mensajeExito = "Usuario creado exitosamente! Revise su correo para verificar su usario.";
+			}
+			else if (!authResponse.huboError)
+			{
+				authResponse.huboError = true;
+				authResponse.mensajeError = "Usuario no registrado. Verifique que haya ingresado un email correcto y una contraseña de mas de 6 caracteres.";
+			}
+		} 
+		catch (error) 
+		{ 
+			console.error("Error en el registro:", error);
+			authResponse.huboError = true;
+			authResponse.mensajeError = `Error desconocido en el registro: ${error}`;
+		}
+		finally { return authResponse; }
 	}
 
 	ObtenerSesion()
