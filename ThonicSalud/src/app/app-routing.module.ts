11880eeb6eda83dcc1fe0612845096140010c779
Thomas-Marino <thomasmarino627@gmail.com>
2024-10-31 19:03:34 -0300

    Implementación del módulo administrador y error con lazy loading

--- ThonicSalud/src/app/app-routing.module.ts
+++ ThonicSalud/src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { IngresoComponent } from './components/ingreso/ingreso.component';
 import { RegistroComponent } from './components/registro/registro.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { usuarioLogueadoGuard } from './guards/usuario-logueado.guard';
 
 const routes: Routes = [
   { path: "", redirectTo: "/landing", pathMatch: "full" },
   { path: "landing", component: LandingPageComponent },
   { path: "ingreso", component: IngresoComponent },
   { path: "registro", component: RegistroComponent },
-  { path: 'error', loadChildren: () => import('./modules/error/error.module').then(m => m.ErrorModule) },
+  { path: "inicio", component: InicioComponent, canActivate: [usuarioLogueadoGuard] },
+  { path: "error", loadChildren: () => import('./modules/error/error.module').then(m => m.ErrorModule) },
+  { path: "administrador", loadChildren: () => import('./modules/administrador/administrador.module').then(m => m.AdministradorModule) },
 ];
 
 @NgModule({
