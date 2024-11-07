96efea379a67d7786464767045025298cf3247ad
Thomas-Marino <thomasmarino627@gmail.com>
2024-11-07 20:11:58 -0300

    Cambio de iconos

--- ThonicSalud/src/app/components/inicio/inicio.component.ts
+++ ThonicSalud/src/app/components/inicio/inicio.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { AuthService } from '../../services/firebase/auth.service';
 import { FirestoreService } from '../../services/firebase/firestore.service';
 import { Router } from '@angular/router';
+import { UserService } from '../../services/data/user.service';
 
 @Component({
   selector: 'app-inicio',
@@ -10,23 +11,16 @@ import { Router } from '@angular/router';
 })
 export class InicioComponent {
   authService: AuthService = inject(AuthService);
+  userService: UserService = inject(UserService);
   firestoreService: FirestoreService = inject(FirestoreService);
   router: Router = inject(Router);
 
-  rolUsuarioLogueado: string = "";
   nombreUsuario: string = "";
 
   constructor() 
   { 
-    this.ObtenerRolUsuarioLogueado();
     this.ObtenerNombreUsuario();
-   }
-  
-  async ObtenerRolUsuarioLogueado(): Promise<void>
-  {
-    const objetoUsuarioLogueado: any = await this.firestoreService.ObtenerUsuarioPorMail(this.authService.ObtenerCorreoUsuario());
-
-    this.rolUsuarioLogueado = objetoUsuarioLogueado.rol;
+    this.userService.ObtenerDatosUsuarioLogueado();
   }
 
   async ObtenerNombreUsuario()
